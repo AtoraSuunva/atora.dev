5f1d722c80058cf5c13eb77c0a147b50782b95ef
AtoraSuunva <atora@giraffeduck.com>
2023-11-16 10:20:27 -0500

    fix: invalid this value in image plugin

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -11,24 +11,27 @@ function relativeToInputPath(inputPath, relativeFilePath) {
 module.exports = (eleventyConfig) => {
   // Eleventy Image shortcode
   // https://www.11ty.dev/docs/plugins/image/
-  eleventyConfig.addAsyncShortcode('image', async (src, alt, widths, sizes) => {
-    // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
-    // Warning: Avif can be resource-intensive so take care!
-    let formats = ['avif', 'webp', 'auto']
-    let file = relativeToInputPath(this.page.inputPath, src)
-    let metadata = await eleventyImage(file, {
-      widths: widths || ['auto'],
-      formats,
-      outputDir: path.join(eleventyConfig.dir.output, 'img'),
-    })
+  eleventyConfig.addAsyncShortcode(
+    'image',
+    async function (src, alt, widths, sizes) {
+      // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+      // Warning: Avif can be resource-intensive so take care!
+      let formats = ['avif', 'webp', 'auto']
+      let file = relativeToInputPath(this.page.inputPath, src)
+      let metadata = await eleventyImage(file, {
+        widths: widths || ['auto'],
+        formats,
+        outputDir: path.join(eleventyConfig.dir.output, 'img'),
+      })
 
-    // TODO loading=eager and fetchpriority=high
-    let imageAttributes = {
-      alt,
-      sizes,
-      loading: 'lazy',
-      decoding: 'async',
-    }
-    return eleventyImage.generateHTML(metadata, imageAttributes)
-  })
+      // TODO loading=eager and fetchpriority=high
+      let imageAttributes = {
+        alt,
+        sizes,
+        loading: 'lazy',
+        decoding: 'async',
+      }
+      return eleventyImage.generateHTML(metadata, imageAttributes)
+    },
+  )
 }
