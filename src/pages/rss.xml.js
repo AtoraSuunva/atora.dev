d4274eb95e93e72bc258475b53f4f62029a5fb49
AtoraSuunva <at@atora.dev>
2025-09-18 16:07:16 +0000

    feat(blog): use /post/ for published posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
-import { isPublishedPost } from '../utils/collections';
+import { getCollection } from 'astro:content'
+import rss from '@astrojs/rss'
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts'
+import { isPublishedPost } from '../utils/collections'
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+  const posts = await getCollection('blog')
 
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.filter(isPublishedPost).map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.filter(isPublishedPost).map((post) => ({
+      ...post.data,
+      link: `/blog/post/${post.id}/`,
+    })),
+  })
 }
